2a68f0c184e0da36c0819cc09809cc7d0b41d090
Hieuuuuu1806 <86762713+Hieuuuuu1806@users.noreply.github.com>
2021-07-01 13:35:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Hello bé PUyen dth nha😏',
+    introDesc: 'Mày phải trả lời thật lòng đó nha con khùng😏',
+    btnIntro: 'ok a HHieu dzai👉🏻👈🏻',
+    title: 'Phải HHieu dzai dth dangiu',
+    desc: 'Nen PUyên mê HHieu lắm đúng hong👉🏻👈🏻',
+    btnYes: 'dzai dr đó aiuuuu ',
+    btnNo: 'Có cái lon nè thằng bị điên',
+    question:'Ủa sao PUyên mê HHieu z😏',
+    btnReply: 'Gửi HHieu dzai iuuu',
+    reply: 'Ai biểu HHieu dzai quá chi làm tui mê muốn xễu',
+    mess: 'Tao bt mà m mê tao lắm t cũng z t cũng mê m😏',
+    messDesc: 'Thế nhớ ế cùng HHieu nha😏',
+    btnAccept: 'Dzaaaaaa',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
